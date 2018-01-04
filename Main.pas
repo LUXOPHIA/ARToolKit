549cfa42479a856ce63813f46ecaa8b98e35f555
luxidea <contact@luxidea.net>
2018-01-04 21:52:53 +0900

    begin

---
+++ Main.pas
@@ -0,0 +1,30 @@
+﻿unit Main;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+  end;
+
+var
+  Form1: TForm1;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+implementation //############################################################### ■
+
+{$R *.fmx}
+
+end. //######################################################################### ■
